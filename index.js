1cae620b8b3366b2269e0b34d40339a99fcc3214
fabian <fabian@asistensi.com>
2021-09-20 11:14:08 -0400

    [IMP] dockerized app

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
-const express = require("express");
-const morgan = require("morgan");
-const cors = require("cors");
+const express = require('express');
 
-/* app and conection */
-const { initServer } = require("./database");
 const app = express();
+const PORT = process.env.PORT || 4000;
 
-app.use(morgan("dev"));
-app.use(cors());
-app.use(express.json());
+app.get('/', (request, response) => {
+  response.status(200).json({
+    message: 'Hello Docker!',
+  });
+});
 
-initServer(app);
+app.listen(PORT, () => {
+  console.log(`Server is up on localhost:${PORT}`);
+});
