90f30444485713a7ed89741e50c5ef1f03a83de9
Fabian <koe7the@gmail.com>
2022-01-13 02:14:04 -0400

    [ADD] get devices components functions

--- handlers/devices/create_device.js
+++ handlers/devices/create_device.js
@@ -2,17 +2,25 @@ const models = require("../../models");
 
 module.exports = (req, res) => {
   const { files, fields: data } = req;
+  console.log("DATA: ", data);
 
-  return new Promise( async (resolve, reject) => {
+  return new Promise(async (resolve, reject) => {
     try {
       let created_device = await models.devices.create({
-        ...data
-      })
+        ...data,
+      });
 
-      return resolve(created_device)
+      let created_deviceComponent =
+        await models.deviceComponent.bulkCreate(
+          JSON.parse(data.components).map((el) => ({
+            component_id: el,
+            device_id: created_device.dataValues.id,
+          }))
+        );
+
+      return resolve({ ...created_device, created_deviceComponent });
     } catch (error) {
-      return reject(error)
+      return reject(error);
     }
-  })
-
-}
+  });
+};
