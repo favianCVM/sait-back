53c268d16a66e7b88a2fe795d29cad60f29a291a
Fabian <fabiancvm7@gmail.com>
2022-01-24 01:39:45 -0400

    technciian profile api

--- handlers/incidences/get_all_incidences.js
+++ handlers/incidences/get_all_incidences.js
@@ -1,5 +1,5 @@
 const models = require("../../models");
-
+const lodash = require("lodash");
 module.exports = (req) => {
   return new Promise(async (resolve, reject) => {
     try {
@@ -50,15 +50,17 @@ module.exports = (req) => {
           ...el.component,
         }));
 
-        item.errors = item.errors.map((el) => ({
-          ...el,
-          components: el.errorComponents.map((il) => ({ ...il.component })),
-        }));
+        item.errors = lodash.sortBy(
+          item.errors.map((el) => ({
+            ...el,
+            components: el.errorComponents.map((il) => ({ ...il.component })),
+          })),
+          (el) => el.id
+        );
 
         acc.push(item);
         return acc;
       }, []);
-
 
       return resolve(output);
     } catch (err) {
